a6de9fd4bb56fd94d4bae4e2a7c5fba1067424e7
peck2703 <peckmr@gmatc.matc.edu>
2016-04-18 10:14:30 -0500

    MainMenuStart

---
+++ Assets/Scripts/Runtime/PauseScript.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScript : MonoBehaviour {
+
+	public bool gamePaused = false;
+
+	public float pauseBoxWidth = 250;
+	public float pauseBoxHeight = 150;
+
+	public float resumeButtonWidth = 100;
+	public float resumeButtonHeight = 25;
+    public float resumeButtonWidthOffset = 0;
+	public float resumeButtonHeightOffset = 25;
+
+    public float restartButtonWidth = 0;
+    public float restartButtonHeight = 0;
+    public float restartButtonWidthOffset = 50;
+    public float restartButtonHeightOffset = 0;
+
+    public float quitButtonWidth = 0;
+	public float quitButtonHeight = -75;
+    public float quitButtonWidthOffset = 50;
+	public float quitButtonHeightOffset = 0;
+
+    public string currentLevel;
+    public string mainMenu;
+	public GUIStyle myStyle;
+
+	void OnGUI()
+	{
+		if (gamePaused) {
+			/*GUI.Box(new Rect((Screen.width / 2) - (pauseBoxWidth / 2),
+			                 (Screen.height / 2) - (pauseBoxHeight / 2),
+			                 pauseBoxWidth, pauseBoxHeight), pauseTexture, myStyle);
+                             */
+			if(GUI.Button(new Rect(((Screen.width / 2) - (resumeButtonWidth / 2))-resumeButtonWidthOffset,
+			                       ((Screen.height / 2) - (resumeButtonHeight / 2)) - resumeButtonHeightOffset,
+			                       resumeButtonWidth, resumeButtonHeight),"Resume"))
+			{
+				Time.timeScale = 1;
+                gamePaused = false;
+			}
+            if (GUI.Button(new Rect(((Screen.width / 2) - (restartButtonWidth / 2)) - restartButtonWidthOffset,
+                                    ((Screen.height / 2) - (restartButtonHeight / 2)) - restartButtonHeightOffset,
+                                    resumeButtonWidth, resumeButtonHeight), "Restart Level"))
+            {
+                Application.LoadLevel(currentLevel);
+                Time.timeScale = 1;
+                
+            }
+
+            if (GUI.Button(new Rect(((Screen.width / 2) - (quitButtonWidth / 2)) - quitButtonWidthOffset,
+                                    ((Screen.height / 2) - (quitButtonHeight / 2)) - quitButtonHeightOffset,
+                                    resumeButtonWidth, resumeButtonHeight), "Quit"))
+            {
+                Application.LoadLevel(0);
+                Time.timeScale = 1;
+            }
+        }
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape) && !gamePaused)
+        {
+            gamePaused = true;
+            Time.timeScale = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) && gamePaused)
+        {
+            gamePaused = false;
+            Time.timeScale = 1;
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/mainGUI.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainGUI : MonoBehaviour
+{
+
+	public string levelSelect;
+	public string nextLevel;
+	public string mainMenu;
+	public Camera mainCamera;
+
+    public Texture starWarsButton;
+    public Texture starTrekButton;
+    public Texture spaceballsButton;
+
+    public bool optionButtonPressed = false;
+	// Use this for initialization
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+	void OnGUI()
+	{
+		if (!optionButtonPressed) {
+			if (GUI.Button (new Rect ((mainCamera.pixelWidth - 200) / 2, (mainCamera.pixelHeight - 250) / 2, 200, 50), starWarsButton))
+            {
+				Application.LoadLevel (nextLevel);
+			}
+			if (GUI.Button (new Rect ((mainCamera.pixelWidth - 200) / 2, (mainCamera.pixelHeight - 125) / 2, 200, 50), starTrekButton))
+            {
+				Application.LoadLevel (levelSelect);
+			}
+			if (GUI.Button (new Rect ((mainCamera.pixelWidth - 200) / 2, (mainCamera.pixelHeight - 0) / 2, 200, 50), spaceballsButton))
+            {
+
+			}
+            if (GUI.Button(new Rect((mainCamera.pixelWidth - 200) / 2, (mainCamera.pixelHeight + 125) / 2, 200, 50), "Options"))
+            { 
+                optionButtonPressed = true;
+            }
+        }
+        else if (optionButtonPressed) {
+            if (GUI.Button(new Rect((mainCamera.pixelWidth - 200) / 2, (mainCamera.pixelHeight - 250) / 2, 200, 50), "Video"))
+            {
+
+            }
+            if (GUI.Button(new Rect((mainCamera.pixelWidth - 200) / 2, (mainCamera.pixelHeight - 125) / 2, 200, 50), "Audio"))
+            {
+
+            }
+            if (GUI.Button(new Rect((mainCamera.pixelWidth - 200) / 2, (mainCamera.pixelHeight - 0) / 2, 200, 50), "Controls"))
+            {
+
+            }
+            if (GUI.Button(new Rect((mainCamera.pixelWidth - 200) / 2, (mainCamera.pixelHeight + 125) / 2, 200, 50), "Back to Main Menu"))
+            {
+                optionButtonPressed = false;
+            }
+            if (GUI.Button())
+            //PlayerPrefs.SetInt("LevelUnlocked")
+        }
+	}
+}
